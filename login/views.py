edf548a4fb8a676360c3753e55cebf11fb203688
Saim Salman <saimsalman36@hotmail.com>
2017-05-03 14:18:27 +0500

    Email Integration

--- login/views.py
+++ login/views.py
@@ -44,7 +44,6 @@ def hostAccountActivationSent(request):
     return HttpResponse()
 
 def hostActivate(request, uidb64, token):
-    print ("In activation")
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
